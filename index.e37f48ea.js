3a84470f1b051718c7317a635858e33e98c5013c
Brandon Knight <knightbrandon1984@gmail.com>
2024-07-20 20:29:02 -0500

    Updates

--- index.e37f48ea.js
+++ index.e37f48ea.js
@@ -6152,6 +6152,7 @@ class ResultsView extends (0, _viewDefault.default) {
         <img
           class="m-0 bg-contain rounded-t-md"
           src="${isImage}"
+          loading="lazy"
           alt="${result.title}"
         />
         <section class="my-0 ml-1.5 content-center text-white text-base tracking-wide">
